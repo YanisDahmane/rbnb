faa6a8cb0ff19fd09433d7274396a5ef0bc82a11
Johan Walter <walter.johan25@gmail.com>
2022-11-23 13:28:53 +0100

    booking controller done

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,6 +3,7 @@ class Room < ApplicationRecord
   belongs_to :category
   belongs_to :user
   belongs_to :address
+  has_many :bookings
   has_one_attached :photo
 
   accepts_nested_attributes_for :address
