12923ba7da8ded43b09d9586cc4b621bf1c7c9c9
Yanis Dahmane <yanis.dahmane.pro@gmail.com>
2022-11-25 14:07:45 +0100

    fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,5 @@ class PagesController < ApplicationController
     @rooms = current_user.rooms
     @bookings = current_user.bookings
     @bookings_request = Booking.where(room: current_user.rooms, confirmed: [0,1])
-    p @bookings_request
   end
 end
