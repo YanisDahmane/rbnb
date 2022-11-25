f58d1c26b0703880d3c0ea788aaed9768bdf743d
Yanis Dahmane <yanis.dahmane.pro@gmail.com>
2022-11-25 15:57:39 +0100

    ok

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,18 +1,23 @@
 class ApiController < ApplicationController
+
+  # Si la demande API est pour une seul room /api/id_room
   def rooms
     render json: create_room_json
   end
 
+  # Si la demande est global /api?params
   def all
     render json: create_all_json
   end
 
+  # Si la demande est sur les categories /api/category
   def category
     render json: category_json
   end
 
   private
 
+  # Création du json pour les categories
   def category_json
     @result = {}
     @categories = Category.all
@@ -22,11 +27,14 @@ class ApiController < ApplicationController
     @result
   end
 
+  # Création du json pour une room (avec toutes les données de celle ci)
   def create_room_json
     @room = Room.find(params[:id])
     @result = {}
     @result[:html] = render_to_string(partial: "pages/room", locals: { room: @room })
     @result[:info] = @room
+
+    # getbooked(@room) permet de recuperer toutes les dates de reservations
     @result[:booked] = getbooked(@room)
     @result[:category] = @room.category.name
     @result[:address] = @room.address
@@ -34,23 +42,23 @@ class ApiController < ApplicationController
     @result
   end
 
+  # Création du json pour toutes les rooms correspondante aux params
   def create_all_json
     @rooms = Room.all
     @result = {}
-    # Localisation params["city"]=
-    # Prix range  params["prix]"=[min,max]
-    # Category params["category"]=
-    # Recherche globale (inclus les mots clés)
     count = 0
+    # On parcours toutes les rooms
     @rooms.each do |room|
+      # On test si la room correspond aux params
       if testRoom(room)
+        # Si oui on l'ajoute au json
         addRoom(room)
-        count += 1
       end
     end
     @result
   end
 
+  # Création du json pour une room
   def addRoom(room)
     @result[room.id] = {}
     @result[room.id][:html] = render_to_string(partial: "pages/room", locals: { room: room })
@@ -60,22 +68,26 @@ class ApiController < ApplicationController
     @result[room.id][:bookings] = Booking.where(room: room)
   end
 
+  # Test si la room correspond aux params
   def testRoom(room)
     return false if params["city"] && params["city"].downcase != room.address.city.downcase
     return false if params["category"] && params["category"] != room.category.name
     return false if params["price_max"] && room.price > params["price_max"].to_i
     return false if params["size_min"] && room.size < params["size_min"].to_i
 
+    # Test de la recherche global
     if params["g"]
+      # Céation d'une string avec toutes les valeurs interessante de la room
       fullinfo = (room.name + room.description + room.category.name + room.address.city).downcase!;
       params["g"].split(',').each do |word|
+        # Si la string ne contient pas le mot on retourne false
         return false unless fullinfo.include?(word.downcase)
       end
     end
     return true
   end
 
-
+  # Récupération de toutes les dates de reservations
   def getbooked(room)
     result = []
     room.bookings.each do |booking|
--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -7,22 +7,27 @@ export default class extends Controller {
   static allRoomApi;
 
   connect() {
-    console.log("ok");
+    // Récupération de toutes les rooms de l'api
     fetch("http://localhost:3000/api")
       .then(response => response.json()
       .then(data => {this.allRoomdata = data}));
   }
 
   find(event){
+    // Création d'une requête pour récupérer les rooms correspondantes aux recherches
     fetch("http://localhost:3000/api?g=" + this.getRequest())
       .then(response => response.json()
       .then(data => {
+        // Suppression des rooms affichées
         this.deckTarget.innerHTML = "";
+        // On boucle sur les rooms récupérées
         for (const [key, value] of Object.entries(data)) {
+          // On affiche les rooms
           this.deckTarget.innerHTML += value["html"];
         }
+        // Si aucune rooms n'est trouvée
         if (this.deckTarget.innerHTML == ""){
-
+          // On affiche un message
           const html = `<div class="booking-card">
           <img class="card-img-top" src="https://cdn.dribbble.com/users/734476/screenshots/4020070/media/30772f1672c8d5f5475cf1066044fb8b.png" alt="not found">
           <div class="booking-card-content">
@@ -35,29 +40,25 @@ export default class extends Controller {
        }));
   }
 
+  // On crée la requête avec les query correspondantes
   getRequest(){
     let result = "";
     if(this.gTarget.value != ""){
-      result += "&g=" + this.gTarget.value;
+      // Recherche globale
+      result += "&g=" + this.gTarget.value.replace(" ", ",");
     }
     if(this.price_maxTarget.value != ""){
+      // Recherche par prix max
       result += "&price_max=" + this.price_maxTarget.value;
     }
     if(this.size_minTarget.value != ""){
+      // Recherche par taille min
       result += "&size_min=" + this.size_minTarget.value;
     }
     if(this.categoryTarget.value != "All categories"){
+      // Recherche par catégorie
       result += "&category=" + this.categoryTarget.value;
     }
     return result;
-  }
-
-  showRooms(param) {
-    this.deckTarget.innerHTML = "";
-    for (const [key, value] of Object.entries(this.allRoomdata)) {
-      if(param == "Tout" || value["category"] == param){
-        this.deckTarget.innerHTML += value["html"];
-      }
-    }
   }
 }
